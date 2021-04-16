28446bcaf069dad84bbdfce337b401303960a6cb
contact.smith.thay <contact.smith.thay@gmail.com>
2021-04-16 08:43:53 -0400

    chekout linking correctly to books and users

--- database/migrations/2021_04_15_185628_create_checkouts_table.php
+++ database/migrations/2021_04_15_185628_create_checkouts_table.php
@@ -16,8 +16,14 @@ class CreateCheckoutsTable extends Migration
         Schema::create('checkouts', function (Blueprint $table) {
             
             $table->id();
-            $table->integer('ref_user_id');
-            $table->integer('ref_book_id');
+          
+            $table->unsignedBigInteger('ref_user_id');
+            $table->foreign('ref_user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('ref_book_id');
+            $table->foreign('ref_book_id')->references('id')->on('books');
+
+            //$table->foreignId('user_id')->constrained();        //integer('ref_user_id');
+           // $table->foreignId('book_id')->constrained();       //integer('ref_book_id');
             $table->datetime("checkout_date");
             $table->datetime("due_date");
             $table->datetime("return_date");
